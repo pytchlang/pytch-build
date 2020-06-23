ae19617bdc6cf1c85c46f76af8c75b0a04c98689
Ben North <ben@redfrontdoor.org>
2020-06-23 13:49:36 +0100

    Add message_subject property

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
@@ -8,3 +8,7 @@ class ProjectCommit:
         self.repo = repo
         self.commit = repo[oid]
         self.oid = self.commit.id
+
+    @cached_property
+    def message_subject(self):
+        return self.commit.message.split('\n')[0]
