69d79196adfda685031455ec99cc7b4ea0136fe1
Ben North <ben@redfrontdoor.org>
2020-06-24 13:02:04 +0100

    ShortcodeProcessor: Add block processor

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_markdown.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_markdown.py
@@ -1,5 +1,25 @@
+import re
+import xml.etree.ElementTree as etree
 import markdown
 from bs4 import BeautifulSoup
+
+
+class ShortcodeProcessor(markdown.blockprocessors.BlockProcessor):
+    RE_SHORTCODE = re.compile(r"^\s*\{\{< (\w+) (.*) >\}\}\s*$")
+
+    def test(self, parent, block):
+        m = self.RE_SHORTCODE.match(block)
+        # TODO: More extensible shortcode mechanism.
+        return (m is not None) and m.group(1) == "commit"
+
+    def run(self, parent, blocks):
+        block = blocks.pop(0)
+        re_match = self.RE_SHORTCODE.match(block)
+
+        # Ignore value; goal is side-effect of attaching to parent:
+        etree.SubElement(parent, "div",
+                         {"class": "patch-hunks",
+                          "data-slug": re_match.group(2)})
 
 
 def soup_from_markdown_text(markdown_text):
