5a8314cfc3d5eb18f9f0f8f99f959a281c42710c
Ben North <ben@redfrontdoor.org>
2020-06-24 11:09:42 +0100

    code_patch_against_parent: Add property

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
@@ -141,3 +141,13 @@ class ProjectCommit:
                     for delta in self.diff_against_parent_or_empty.deltas]
         else:
             return []
+
+    @cached_property
+    def code_patch_against_parent(self):
+        if not self.modifies_python_code:
+            raise ValueError(f"commit {self.oid} does not modify the Python code")
+
+        delta = self.sole_modify_against_parent
+        old_blob = self.repo[delta.old_file.id]
+        new_blob = self.repo[delta.new_file.id]
+        return old_blob.diff(new_blob)
