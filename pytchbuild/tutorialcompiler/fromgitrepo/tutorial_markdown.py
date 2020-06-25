ad8ab0c77ce128adb86faef9b16422f3b23706b0
Ben North <ben@redfrontdoor.org>
2020-06-25 11:51:19 +0100

    soup_from_markdown_text(): Add basic version
    Will augment in due course to handle our shortcodes.

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_markdown.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_markdown.py
@@ -1,2 +1,8 @@
 import markdown
 from bs4 import BeautifulSoup
+
+
+def soup_from_markdown_text(markdown_text):
+    html = markdown.markdown(markdown_text)
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
