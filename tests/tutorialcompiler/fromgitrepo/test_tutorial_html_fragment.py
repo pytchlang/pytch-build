770003415b21788255af62f927ac0625c920e5de
Ben North <ben@redfrontdoor.org>
2020-08-04 16:09:32 +0100

    MockHunkLine: Add test helper class

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_html_fragment.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_html_fragment.py
@@ -1,3 +1,11 @@
 import pytest
+from dataclasses import dataclass
 
 import pytchbuild.tutorialcompiler.fromgitrepo.tutorial_html_fragment as THF
+
+
+@dataclass
+class MockHunkLine:
+    old_lineno: int
+    new_lineno: int
+    content: str
