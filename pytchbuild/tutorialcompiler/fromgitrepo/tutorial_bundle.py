30eca5eb33df2860768a26ffa087e885c3e59c90
Ben North <ben@redfrontdoor.org>
2020-08-04 16:05:31 +0100

    from_project_history(): Add classmethod

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_bundle.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_bundle.py
@@ -3,9 +3,17 @@ from typing import List
 import bs4
 
 from .tutorial_history import ProjectAsset
+from .tutorial_html_fragment import div_from_project_history
 
 
 @dataclass
 class TutorialBundle:
     html_fragment: bs4.element.Tag
     assets: List[ProjectAsset]
+
+    @classmethod
+    def from_project_history(cls, project_history):
+        return cls(
+            div_from_project_history(project_history),
+            project_history.all_project_assets
+        )
