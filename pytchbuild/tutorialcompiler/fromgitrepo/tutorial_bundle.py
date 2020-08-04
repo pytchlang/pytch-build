25e2df7c76a272d38f2ae8097afe450d62396ce3
Ben North <ben@redfrontdoor.org>
2020-08-04 19:33:11 +0100

    write_zipfile(): Add method

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_bundle.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_bundle.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import List
+from contextlib import closing
 from pathlib import Path
+import zipfile
 import bs4
 
 from .tutorial_history import ProjectAsset
@@ -20,3 +22,13 @@ class TutorialBundle:
             div_from_project_history(project_history),
             project_history.all_project_assets
         )
+
+    def write_zipfile(self, out_file):
+        with closing(zipfile.ZipFile(out_file, mode="w")) as zfile:
+            bundle_root_path = Path(self.top_level_directory_name)
+            tutorial_html_path = bundle_root_path / "tutorial.html"
+            tutorial_html_bytes = self.html_fragment.encode("utf-8")
+            zfile.writestr(str(tutorial_html_path), tutorial_html_bytes)
+
+            for asset in self.assets:
+                zfile.writestr(asset.path, asset.data)
