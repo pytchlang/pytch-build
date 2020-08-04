0e1cbf6342b38b8f716d22e9cc2608d0c2c83ece
Ben North <ben@redfrontdoor.org>
2020-08-04 19:40:28 +0100

    Store top-level directory name

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_bundle.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_bundle.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+from pathlib import Path
 import bs4
 
 from .tutorial_history import ProjectAsset
@@ -8,12 +9,14 @@ from .tutorial_html_fragment import div_from_project_history
 
 @dataclass
 class TutorialBundle:
+    top_level_directory_name: Path
     html_fragment: bs4.element.Tag
     assets: List[ProjectAsset]
 
     @classmethod
     def from_project_history(cls, project_history):
         return cls(
+            Path(project_history.top_level_directory_name),
             div_from_project_history(project_history),
             project_history.all_project_assets
         )
