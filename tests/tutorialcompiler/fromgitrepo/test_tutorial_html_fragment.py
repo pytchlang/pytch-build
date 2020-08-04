0800d68c162afe8c17406dc7162d797565dcf72d
Ben North <ben@redfrontdoor.org>
2020-08-04 16:10:24 +0100

    line_classification(): Test

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_html_fragment.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_html_fragment.py
@@ -9,3 +9,17 @@ class MockHunkLine:
     old_lineno: int
     new_lineno: int
     content: str
+
+
+class TestHunkTable:
+    @pytest.mark.parametrize(
+        'old_lineno,new_lineno,exp_class',
+        [
+            (10, 12, 'diff-unch'),
+            (-1, 8, 'diff-add'),
+            (12, -1, 'diff-del')
+        ])
+    def test_line_classification(self, old_lineno, new_lineno, exp_class):
+        mock_hunk_line = MockHunkLine(old_lineno, new_lineno, 'ignored')
+        got_class = THF.line_classification(mock_hunk_line)
+        assert got_class == exp_class
