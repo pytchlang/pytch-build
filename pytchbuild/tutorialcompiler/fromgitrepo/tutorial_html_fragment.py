47e686fb0acde9d5afe95a1b646c0be1408cc80b
Ben North <ben@redfrontdoor.org>
2020-08-04 16:13:01 +0100

    table_row_from_line(): Add function

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_html_fragment.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_html_fragment.py
@@ -13,3 +13,18 @@ def table_data_from_line_number(soup, lineno):
     if lineno != -1:
         cell.append(str(lineno))
     return cell
+
+
+def table_row_from_line(soup, line):
+    row = soup.new_tag("tr", attrs={"class": line_classification(line)})
+    content_cell = soup.new_tag("td")
+    content_pre = soup.new_tag("pre")
+
+    row.append(table_data_from_line_number(soup, line.old_lineno))
+    row.append(table_data_from_line_number(soup, line.new_lineno))
+
+    content_pre.append(line.content.rstrip("\n"))
+    content_cell.append(content_pre)
+    row.append(content_cell)
+
+    return row
