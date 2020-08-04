6bab2f3fb09ad0704ee77b5ffd82541641e67c94
Ben North <ben@redfrontdoor.org>
2020-08-04 16:10:49 +0100

    table_data_from_line_number(): Add function

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_html_fragment.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_html_fragment.py
@@ -6,3 +6,10 @@ def line_classification(hunk_line):
     return ("diff-add" if hunk_line.old_lineno == -1
             else "diff-del" if hunk_line.new_lineno == -1
             else "diff-unch")
+
+
+def table_data_from_line_number(soup, lineno):
+    cell = soup.new_tag("td")
+    if lineno != -1:
+        cell.append(str(lineno))
+    return cell
