fdc2f91ea6b5a4d5c6a9a8678bd4f32cf36087f4
Ben North <ben@redfrontdoor.org>
2020-08-04 16:08:35 +0100

    line_classification(): Add function

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_html_fragment.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_html_fragment.py
@@ -1,2 +1,8 @@
 """Create an *HTML soup* of the tutorial
 """
+
+
+def line_classification(hunk_line):
+    return ("diff-add" if hunk_line.old_lineno == -1
+            else "diff-del" if hunk_line.new_lineno == -1
+            else "diff-unch")
