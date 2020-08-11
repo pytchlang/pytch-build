a174e505525a96dc2e4fbb157b98afa2271dde65
Ben North <ben@redfrontdoor.org>
2020-08-11 16:00:51 +0100

    (Introduce variable and reformat)

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_html_fragment.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_html_fragment.py
@@ -163,8 +163,11 @@ class TestHtmlFragment:
         # It's possible this will fail one day if I'm making unwarranted
         # assumptions about the order in which attributes are represented
         # in the string form of an HTML fragment.
-        assert str(THF.div_from_front_matter(soup, front_matter, code)) == (
-            '<div class="front-matter" data-complete-code-text="foo()">'
+        got_div = THF.div_from_front_matter(soup, front_matter, code)
+        assert str(got_div) == (
+            '<div class="front-matter"'
+            ' data-complete-code-text="foo()"'
+            '>'
             '<p>hello</p>'
             '<p>world</p>'
             '</div>'
