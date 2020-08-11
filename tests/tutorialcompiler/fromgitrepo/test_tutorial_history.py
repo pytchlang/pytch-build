a52bcaf522e0b1a49abda6a0e59a8436cd379678
Ben North <ben@redfrontdoor.org>
2020-08-11 16:04:05 +0100

    ProjectHistory.initial_code_text: Test

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_history.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_history.py
@@ -172,6 +172,9 @@ class TestProjectHistory:
     def test_tutorial_text(self, project_history):
         assert re.match(r"# Boing!", project_history.tutorial_text)
 
+    def test_initial_code_text(self, project_history):
+        assert project_history.initial_code_text == ""
+
     def test_final_code_text(self, project_history):
         assert re.match(r"import pytch", project_history.final_code_text)
 
