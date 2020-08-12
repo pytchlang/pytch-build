91d73ac778759cf4adb3d8073a93f323785cbf21
Ben North <ben@redfrontdoor.org>
2020-08-12 12:19:34 +0100

    test_shortcode: Skip if text-source is workdir

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
@@ -3,6 +3,8 @@ import pytchbuild.tutorialcompiler.fromgitrepo.tutorial_markdown as TM
 
 class TestShortcodeParsing:
     def test_shortcode(self, tutorial_md_text):
+        if tutorial_md_text.startswith("Working copy"):
+            return
         soup = TM.soup_from_markdown_text(tutorial_md_text)
         all_maybe_slugs = [elt.get("data-slug", None) for elt in soup.find_all()]
         all_mentioned_slugs = [s for s in all_maybe_slugs if s is not None]
