3940939914c98f173834e4f006c9a460af906a0b
Ben North <ben@redfrontdoor.org>
2020-09-13 13:57:29 +0100

    dev-server-pieces.sh: Add
    Calls further scripts for the individual pieces, to be added shortly.

---
+++ makesite/local-server/dev-server-pieces.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PYTCH_REPO_BASE="$(realpath "$(dirname "$0")"/../../..)"
+PYTCH_LOCAL_SERVER_DIR="$(realpath "$(dirname "$0")")"
+
+tmux split-window -d -b -v \
+     env \
+     PYTCH_REPO_BASE="$PYTCH_REPO_BASE" \
+     PUBLIC_URL="" \
+     REACT_APP_SKULPT_BASE=http://localhost:8124 \
+     REACT_APP_TUTORIALS_BASE=http://localhost:8125 \
+     "$PYTCH_LOCAL_SERVER_DIR"/dev-server-webapp.sh
+
+tmux split-window -h \
+     env \
+     PYTCH_REPO_BASE="$PYTCH_REPO_BASE" \
+     PYTCH_LOCAL_SERVER_DIR="$PYTCH_LOCAL_SERVER_DIR" \
+     "$PYTCH_LOCAL_SERVER_DIR"/dev-server-tutorials.sh
+
+exec \
+     env \
+     PYTCH_REPO_BASE="$PYTCH_REPO_BASE" \
+     PYTCH_LOCAL_SERVER_DIR="$PYTCH_LOCAL_SERVER_DIR" \
+     "$PYTCH_LOCAL_SERVER_DIR"/dev-server-skulpt.sh
+
+# TODO: How to integrate the live-reload/push of tutorial content with
+# the React App?
