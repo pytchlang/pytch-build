7a909e316a8565b6977cddf90e7f2f7602548340
Ben North <ben@redfrontdoor.org>
2020-09-30 13:36:29 +0100

    Add tmux piece script to launch live-reload server

---
+++ makesite/local-server/dev-server-live-reload-watch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. "$PYTCH_REPO_BASE"/pytch-build/venv/bin/activate
+cd "$PYTCH_REPO_BASE"/pytch-tutorials
+
+if [ -z "$PYTCH_IN_PROGRESS_TUTORIAL" ]; then
+    echo "PYTCH_IN_PROGRESS_TUTORIAL not set; not watching any tutorial/code"
+
+    # Keep this script running, to stop the tmux pane from vanishing:
+    while /bin/true; do
+        sleep 60
+    done
+fi
+
+echo "Watching tutorial $PYTCH_IN_PROGRESS_TUTORIAL"
+exec pytchbuild-watch "$PYTCH_IN_PROGRESS_TUTORIAL"
