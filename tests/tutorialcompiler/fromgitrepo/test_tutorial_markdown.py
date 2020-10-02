1a71f7f83abce43aa6be40e994167900542bd3cc
Ben North <ben@redfrontdoor.org>
2020-10-02 21:56:41 +0100

    ordered_commit_slugs_in_soup(): Test
    (Not for "working copy" tutorial text.)

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
@@ -9,3 +9,10 @@ class TestShortcodeParsing:
         all_maybe_slugs = [elt.get("data-slug", None) for elt in soup.find_all()]
         all_mentioned_slugs = [s for s in all_maybe_slugs if s is not None]
         assert all_mentioned_slugs == ["import-pytch", "add-Alien-skeleton"]
+
+    def test_ordered_commit_slugs_in_soup(self, tutorial_md_text):
+        if tutorial_md_text.startswith("Working copy"):
+            return
+        soup = TM.soup_from_markdown_text(tutorial_md_text)
+        ordered_commit_slugs = TM.ordered_commit_slugs_in_soup(soup)
+        assert ordered_commit_slugs == ["import-pytch", "add-Alien-skeleton"]
