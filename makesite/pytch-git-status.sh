a39d912e6b8e889bf519cb9c69d2b077830b1ed0
Ben North <ben@redfrontdoor.org>
2020-11-10 23:38:42 +0000

    Include head SHA1 in output per repo

--- makesite/pytch-git-status.sh
+++ makesite/pytch-git-status.sh
@@ -12,7 +12,7 @@ export PYTCH_REPOS_BASE=$(realpath "$THIS_DIR"/../..)
             tput bold
             echo ------------------------------------------------------------------------
             echo
-            echo $x
+            printf "%-16s  %s\n" "$x" $(git rev-parse HEAD)
             echo
             tput sgr0
             git status
