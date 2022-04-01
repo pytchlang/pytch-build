e9cb47c23e66ec2b3c6f8f9fde74a891eef036c0
Ben North <ben@redfrontdoor.org>
2022-04-01 09:34:37 +0100

    test_when_is_clean(): Add

--- tests/tutorialcompiler/fromgitrepo/test_repo_functions.py
+++ tests/tutorialcompiler/fromgitrepo/test_repo_functions.py
@@ -1,1 +1,7 @@
 import pytchbuild.tutorialcompiler.fromgitrepo.repo_functions as TCRF
+
+
+class TestEnsureStatusClean:
+    def test_when_is_clean(self, clean_cloned_repo):
+        TCRF.ensure_status_clean(clean_cloned_repo)
+        # If we get here without exception, test has passed.
