880f5214fe20403a0d97b2e5dabff250e990732b
Ben North <ben@redfrontdoor.org>
2022-04-01 09:16:05 +0100

    ensure_status_clean(): Add

--- pytchbuild/tutorialcompiler/fromgitrepo/repo_functions.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/repo_functions.py
@@ -8,3 +8,18 @@ def create_signature(repo):
         repo.config["user.email"],
         time=int(time.time()),
     )
+
+
+def ensure_status_clean(repo):
+    acceptable_flags_values = [
+        pygit2.GIT_STATUS_CURRENT,
+        pygit2.GIT_STATUS_IGNORED,
+    ]
+
+    unclean_paths = []
+    for path, flags in repo.status().items():
+        if flags not in acceptable_flags_values:
+            unclean_paths.append(path)
+
+    if unclean_paths:
+        raise RuntimeError(f'repo not clean: {", ".join(unclean_paths)}')
