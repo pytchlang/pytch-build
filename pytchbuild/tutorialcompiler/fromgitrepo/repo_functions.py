3b89df1d6a24198c896f7402e1117376e08b1aea
Ben North <ben@redfrontdoor.org>
2022-04-01 09:41:11 +0100

    commit_files(): Add

--- pytchbuild/tutorialcompiler/fromgitrepo/repo_functions.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/repo_functions.py
@@ -23,3 +23,16 @@ def ensure_status_clean(repo):
 
     if unclean_paths:
         raise RuntimeError(f'repo not clean: {", ".join(unclean_paths)}')
+
+
+def commit_files(repo, filenames, commit_sig, commit_message):
+    index = repo.index
+    for filename in filenames:
+        index.add(filename)
+    index.write()
+    tree = index.write_tree()
+    repo.create_commit(repo.head.name,
+                       commit_sig, commit_sig,
+                       commit_message,
+                       tree,
+                       [repo.head.target])
