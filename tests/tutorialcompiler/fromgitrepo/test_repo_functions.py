26d01611c0ab421c0baafdf380c4768a4a6a6bef
Ben North <ben@redfrontdoor.org>
2022-04-01 09:34:59 +0100

    test_when_modified_files(): Add

--- tests/tutorialcompiler/fromgitrepo/test_repo_functions.py
+++ tests/tutorialcompiler/fromgitrepo/test_repo_functions.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pytchbuild.tutorialcompiler.fromgitrepo.repo_functions as TCRF
 
 
@@ -5,3 +7,8 @@ class TestEnsureStatusClean:
     def test_when_is_clean(self, clean_cloned_repo):
         TCRF.ensure_status_clean(clean_cloned_repo)
         # If we get here without exception, test has passed.
+
+    def test_when_modified_files(self, cloned_repo):
+        # The cloned_repo fixture modifies two files.
+        with pytest.raises(RuntimeError, match="repo not clean"):
+            TCRF.ensure_status_clean(cloned_repo)
