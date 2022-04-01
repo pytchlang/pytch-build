92bae5fe0ace46224028747aad98014fcf00b5a6
Ben North <ben@redfrontdoor.org>
2022-04-01 09:11:31 +0100

    NEW_BRANCH_STARTING_COMMIT: Define constant

--- pytchbuild/tutorialcompiler/fromgitrepo/config.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/config.py
@@ -4,3 +4,4 @@ Constants for working with git-repo-based tutorial collections
 
 RELEASE_RECIPES_BRANCH_NAME = "release-recipes"
 RELEASES_BRANCH_NAME = "releases"
+NEW_BRANCH_STARTING_COMMIT = "initial-empty-state"
