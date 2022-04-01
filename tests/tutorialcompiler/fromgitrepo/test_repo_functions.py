b1581bfb410128495434804abd29e9c1f15a149c
Ben North <ben@redfrontdoor.org>
2022-04-01 09:35:09 +0100

    test_when_untracked_files(): Add

--- tests/tutorialcompiler/fromgitrepo/test_repo_functions.py
+++ tests/tutorialcompiler/fromgitrepo/test_repo_functions.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 
 import pytchbuild.tutorialcompiler.fromgitrepo.repo_functions as TCRF
 
@@ -12,3 +13,10 @@ class TestEnsureStatusClean:
         # The cloned_repo fixture modifies two files.
         with pytest.raises(RuntimeError, match="repo not clean"):
             TCRF.ensure_status_clean(cloned_repo)
+
+    def test_when_untracked_files(self, clean_cloned_repo):
+        new_file_path = Path(clean_cloned_repo.workdir) / "hello.txt"
+        with new_file_path.open("wt") as f_out:
+            f_out.write("Hello world!\n")
+        with pytest.raises(RuntimeError, match="repo not clean"):
+            TCRF.ensure_status_clean(clean_cloned_repo)
