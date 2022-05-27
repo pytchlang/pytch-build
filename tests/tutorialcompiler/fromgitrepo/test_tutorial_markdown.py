bae75298206ef71bee517a2ba7ff91abed05e935
justinemoulin <justinemoulin5@icloud.com>
2022-05-27 13:57:46 +0100

    Add test_soup_from_markdown_text()

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
@@ -16,3 +16,14 @@ class TestShortcodeParsing:
         soup = TM.soup_from_markdown_text(tutorial_md_text)
         ordered_commit_slugs = TM.ordered_commit_slugs_in_soup(soup)
         assert ordered_commit_slugs == ["import-pytch", "add-Alien-skeleton"]
+    
+    def test_soup_from_markdown_text(self, tutorial_md_text):
+        if tutorial_md_text.startswith("Working copy"):
+            return
+        soup = TM.soup_from_markdown_text(tutorial_md_text)
+        print(soup)
+        all_maybe_slugs = [elt.get_text() for elt in soup.find_all('code')]
+        print(all_maybe_slugs)
+        all_mentioned_slugs = [s for s in all_maybe_slugs if s is not None]
+        assert all_mentioned_slugs == ['go to x: [0] y: [120]\n']
+        
