0a63030906bfbacec9a4751ab5525baa91a4e512
justinemoulin <justinemoulin5@icloud.com>
2022-05-31 12:58:07 +0100

    Change name to test_scratchblocks_code()

--- tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
+++ tests/tutorialcompiler/fromgitrepo/test_tutorial_markdown.py
@@ -17,7 +17,7 @@ class TestShortcodeParsing:
         ordered_commit_slugs = TM.ordered_commit_slugs_in_soup(soup)
         assert ordered_commit_slugs == ["import-pytch", "add-Alien-skeleton"]
     
-    def test_soup_from_markdown_text(self, tutorial_md_text):
+def test_scratchblocks_code(self, tutorial_md_text):
         if tutorial_md_text.startswith("Working copy"):
             return
         soup = TM.soup_from_markdown_text(tutorial_md_text)
