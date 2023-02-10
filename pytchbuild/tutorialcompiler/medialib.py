752cadea431ff8eda3c442328b15386ec746bda4
Ben North <ben@redfrontdoor.org>
2023-02-10 12:06:26 +0000

    MediaLibraryEntry.as_output_dict(): Add

--- pytchbuild/tutorialcompiler/medialib.py
+++ pytchbuild/tutorialcompiler/medialib.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import List
 
 
@@ -19,3 +19,12 @@ class MediaLibraryEntry:
     name: str
     items: List[MediaLibraryItem]
     tags: List[str]
+
+    def as_output_dict(self):
+        items_dicts = [asdict(item) for item in self.items]
+        return {
+            "id": self.id,
+            "name": self.name,
+            "items": items_dicts,
+            "tags": self.tags,
+        }
