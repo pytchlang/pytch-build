395ed292d548adb6cabe2f52f7d0e0ed29a9164f
Ben North <ben@redfrontdoor.org>
2023-02-10 12:01:38 +0000

    MediaLibraryEntry: Add dataclass

--- pytchbuild/tutorialcompiler/medialib.py
+++ pytchbuild/tutorialcompiler/medialib.py
@@ -9,3 +9,13 @@ class MediaLibraryItem:
     name: str
     relativeUrl: str
     size: List[int]
+
+
+@dataclass
+class MediaLibraryEntry:
+    # The field names end up as JSON, and ultimately as properties of
+    # the front-end type "ClipArtGalleryEntry".
+    id: int
+    name: str
+    items: List[MediaLibraryItem]
+    tags: List[str]
