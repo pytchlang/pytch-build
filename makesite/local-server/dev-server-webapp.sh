736e8c8e870b01b49d64780680b3b62602291d70
Ben North <ben@redfrontdoor.org>
2023-04-21 11:47:06 +0100

    dev-server-webapp.sh: Source .env file

--- makesite/local-server/dev-server-webapp.sh
+++ makesite/local-server/dev-server-webapp.sh
@@ -3,6 +3,19 @@
 cd_or_fail() { cd "$1" || exit 1; }
 
 cd_or_fail "$PYTCH_REPO_BASE"/pytch-webapp
+
+dotenvfile=./src/.env
+if [ ! -r ${dotenvfile} ]; then
+    echo No .env file in src
+    sleep 60
+    exit 1
+fi
+
+set -o allexport
+# shellcheck disable=SC1090
+source ${dotenvfile}
+set +o allexport
+
 npm start
 
 # Keep the shell process running in case of error, so tmux doesn't
