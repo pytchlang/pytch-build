pytchlang/pytch-tutorials

{#move-clamp-bowl-left}

catch-apple/code.py
@@ -9,6 +9,9 @@ class Bowl(pytch.Sprite):
         self.go_to_xy(0, -145)
 
         while True:
+            if pytch.key_is_pressed("a"):
+                if self.get_x() >= -145:
+                    self.change_x(-2)
             if pytch.key_is_pressed("d"):
                 if self.get_x() <= 190:
                     self.change_x(2)

pytchlang/pytch-build

catch-apple/code.py
@@ -9,6 +9,9 @@ class Bowl(pytch.Sprite):
         self.go_to_xy(0, -145)
 
         while True:
+            if pytch.key_pressed("a"):
+                if self.x_position >= -145:
+                    self.change_x(-2)
             if pytch.key_pressed("d"):
                 if self.x_position <= 190:
                     self.change_x(2)