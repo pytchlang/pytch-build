pytchlang/pytch-tutorials

{#move-bowl-right}

catch-apple/code.py
@@ -7,3 +7,7 @@ class Bowl(pytch.Sprite):
     @pytch.when_green_flag_clicked
     def move_with_keys(self):
         self.go_to_xy(0, -145)
+
+        while True:
+            if pytch.key_is_pressed("d"):
+                self.change_x(2)

pytchlang/pytch-build

catch-apple/code.py
@@ -7,3 +7,7 @@ class Bowl(pytch.Sprite):
     @pytch.when_green_flag_clicked
     def move_with_keys(self):
         self.go_to_xy(0, -145)
+
+        while True:
+            if pytch.key_pressed("d"):
+                self.change_x(2)