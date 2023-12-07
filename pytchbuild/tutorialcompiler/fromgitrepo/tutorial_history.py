0b7bb4b333cd134b57542fbc052383afe3ec317e
Ben North <ben.north@tcd.ie>
2023-12-07 13:57:53 +0000

    code_patch_against_parent: Simplify

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
@@ -397,11 +397,7 @@ class ProjectCommit:
 
     @cached_property
     def code_patch_against_parent(self):
-        if not self.modifies_python_code:
-            raise TutorialStructureError(
-                f"commit {self.oid} does not modify the Python code"
-            )
-
+        self.assert_modifies_python_code()
         delta = self.sole_modify_against_parent
         old_blob = self.repo[delta.old_file.id]
         new_blob = self.repo[delta.new_file.id]
