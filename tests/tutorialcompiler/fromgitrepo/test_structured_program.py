146ededa0c691354cff86cf8420973639bd18cec
Ben North <ben@redfrontdoor.org>
2023-12-10 16:06:52 +0000

    sole_decorator_of_func_def(): Define helper

--- tests/tutorialcompiler/fromgitrepo/test_structured_program.py
+++ tests/tutorialcompiler/fromgitrepo/test_structured_program.py
@@ -25,3 +25,9 @@ def func_defs_of_path(relative_path):
 def class_defs_of_path(relative_path):
     code_ast = fixture_code_ast(relative_path)
     return statements_of_kind(code_ast, ast.ClassDef)
+
+
+def sole_decorator_of_func_def(func_def):
+    decorators = func_def.decorator_list
+    assert len(decorators) == 1
+    return decorators[0]
