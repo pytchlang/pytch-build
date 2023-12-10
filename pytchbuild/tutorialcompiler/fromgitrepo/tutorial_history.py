e98185d22a8b27877ba91effbac2c4aed113737b
Ben North <ben@redfrontdoor.org>
2023-12-10 15:40:23 +0000

    old_and_new_blobs: Define property

--- pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
+++ pytchbuild/tutorialcompiler/fromgitrepo/tutorial_history.py
@@ -396,6 +396,14 @@ class ProjectCommit:
             )
 
     @cached_property
+    def old_and_new_blobs(self):
+        self.assert_modifies_python_code()
+        delta = self.sole_modify_against_parent
+        old_blob = self.repo[delta.old_file.id]
+        new_blob = self.repo[delta.new_file.id]
+        return old_blob, new_blob
+
+    @cached_property
     def code_patch_against_parent(self):
         self.assert_modifies_python_code()
         delta = self.sole_modify_against_parent
