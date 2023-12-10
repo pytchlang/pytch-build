ec422b72d4cc2eb0a414feb38ab4a4ef2e14c6be
Ben North <ben@redfrontdoor.org>
2023-12-10 16:04:38 +0000

    fixture_code_ast(): Define helper

--- tests/tutorialcompiler/fromgitrepo/test_structured_program.py
+++ tests/tutorialcompiler/fromgitrepo/test_structured_program.py
@@ -1,8 +1,13 @@
 import pytest
 from pathlib import Path
+import ast
 
 
 def fixture_code_text(relative_path):
     this_dir = Path(__file__).parent
     full_path = this_dir / "structured-program-fixtures" / relative_path
     return full_path.open("rt").read()
+
+
+def fixture_code_ast(relative_path):
+    return ast.parse(fixture_code_text(relative_path))
