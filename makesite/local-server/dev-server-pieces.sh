b790350792b016947d2cea9e93a72588bc4d4cf8
Ben North <ben.north@tcd.ie>
2024-08-30 14:29:35 +0100

    VITE_STATIC_BLOBS_BASE: Set env.var for local dev

--- makesite/local-server/dev-server-pieces.sh
+++ makesite/local-server/dev-server-pieces.sh
@@ -9,6 +9,7 @@ tmux split-window -d -b -v \
      env \
      PYTCH_REPO_BASE="$PYTCH_REPO_BASE" \
      VITE_DEPLOY_BASE_URL="" \
+     VITE_STATIC_BLOBS_BASE=http://localhost:8129 \
      VITE_SKULPT_BASE=http://localhost:8124 \
      VITE_TUTORIALS_BASE=http://localhost:8125 \
      VITE_DEMOS_BASE=http://localhost:8126 \
