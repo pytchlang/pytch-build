94ffbe48d07788ce75073b45edba8247ffe80da7
Ben North <ben.north@tcd.ie>
2024-12-19 12:26:22 +0000

    Rename variable to shorten line

--- tests/tutorialcompiler/fromgitrepo/test_medialib.py
+++ tests/tutorialcompiler/fromgitrepo/test_medialib.py
@@ -18,7 +18,7 @@ block_singleton = MLib.MediaLibraryEntry(1001, "block", [block], ["block"])
 other_block_singleton = MLib.MediaLibraryEntry(1002, "block", [block], ["cube"])
 fruit_entry = MLib.MediaLibraryEntry(1003, "fruit", [banana, apple], ["fruit", "food"])
 other_fruit_entry = MLib.MediaLibraryEntry(1004, "fruit", [banana, apple], ["healthy"])
-other_healthy_entry = MLib.MediaLibraryEntry(1005, "healthy-foods", [banana, apple], ["healthy"])
+healthy_entry = MLib.MediaLibraryEntry(1005, "healthy-foods", [banana, apple], ["healthy"])
 
 entries = [
     block_singleton,
@@ -26,7 +26,7 @@ entries = [
     other_fruit_entry,
     MLib.MediaLibraryEntry(1006, "animals", [cow, horse], ["farm", "animal"]),
     other_block_singleton,
-    other_healthy_entry,
+    healthy_entry,
 ]
 
 
